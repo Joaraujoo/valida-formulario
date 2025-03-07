94cd6f99cf3bbb53749e48cc15175c5bf74715d1
Joao Araujo <163578593+Joaraujoo@users.noreply.github.com>
2025-03-07 17:29:39 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,17 +7,18 @@ class ValidaFormulario {
 
     eventos() {
         this.formulario.addEventListener('submit', e => {
-            this.handleSubmit(e)
+        this.handleSubmit(e)
         } )
     }
 
     handleSubmit(e){
         e.preventDefault()
-        const validInputs = this.checkInputs()
-        const senhasValidas = this.senhasIguais()
+        this.checkInputs()
+        this.senhasIguais()
 
     }
 
+    //verifica se as senhas estão iguais
     senhasIguais(){
         let valid = true
 
@@ -38,30 +39,42 @@ class ValidaFormulario {
         return valid
 
     }
-    
+
     checkInputs(){
         let valid = true
 
+        // Remove mensagens de erro anteriores
         for(let errorText of this.formulario.querySelectorAll('.error-text')) {
             errorText.remove()
         }
 
         for(let campo of this.formulario.querySelectorAll('.validar')) {
+            //Obtém o elemento irmão anterior ao campo atual no HTML
             const label = campo.previousElementSibling.innerText
 
+             // Verifica se o campo está vazio
             if(!campo.value) {
                 this.createError(campo, `Campo "${label}" não pode estar em branco`)
                 valid = false
             }
 
+             // Valida usuário
             if(campo.classList.contains('usuario')) {
                 if(!this.validaUsuario(campo)) valid = false
             }
 
+             // Valida e-mail
+            if(campo.classList.contains('email')) {
+                if(!this.validaEmail(campo)) valid = false
+            }
+
+             // Valida CPF
             if(campo.classList.contains('cpf')) {
                 
-                if(campo.value.length != 11) {
-                    this.createError(campo, 'O CPF precisa ser no formato xxx.xxx.xxx-xx')
+                const cpfNumeros = campo.value.replace(/\D+/g, '')
+                if (cpfNumeros.length !== 11) {
+                    this.createError(campo, 'O CPF precisa ter 11 números (xxx.xxx.xxx-xx)');
+                    valid = false;
                 }
             }
 
@@ -70,6 +83,21 @@ class ValidaFormulario {
         return valid;
     }
 
+    //Verifica se o email esta com o formato padrão 
+    validaEmail(campo) {
+        const email = campo.value
+        let valid = true
+
+        //expressão regular
+        if(!email.match(/^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/)) {
+            this.createError(campo, 'Email inválido')
+            valid = false
+        }
+
+        return valid
+    }
+
+    //Verifica se o usuario atende os requesitos solicitados 
     validaUsuario(campo){
         const usuario = campo.value
         let valid = true
@@ -80,6 +108,7 @@ class ValidaFormulario {
             valid = false
         }
 
+        //expressão regular
         if(!usuario.match(/^[a-zA-z0-9]+$/g)) {
             this.createError(campo, 'Nome de usuário precisa conter apenas letras e/ou números')
             valid = false
@@ -88,10 +117,12 @@ class ValidaFormulario {
         return valid
     }
 
+    //Cria a mensagem de ERRO e imprimi no HTML
     createError(campo, msg){
         const div = document.createElement('div')
         div.innerHTML = msg
         div.classList.add('error-text')
+        //insere o elemento HTML em uma posição específica
         campo.insertAdjacentElement('afterend', div)
     }
 
